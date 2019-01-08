ec3117f3fbb91ff467864602ce59efaecaac19fc
unixpunk <steve@hackode.com>
2019-01-08 00:15:29 -0600

    Added status echos

--- commit.sh
+++ commit.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+echo "Copying .config to PlutoWeb/plutoweb/zynq_pluto_defconfig..."
+cp buildroot/.config ../PlutoWeb/plutoweb/zynq_pluto_defconfig
 cd build
+echo "Copying boot.dfu boot.frm pluto.frm pluto.dfu to PlutoWeb/plutoweb/images/..."
 cp boot.dfu boot.frm pluto.frm pluto.dfu ../../PlutoWeb/plutoweb/images/
