3512e8cd11aca23637a5fa3058ad346759e87fc2
unixpunk <steve@hackode.com>
2019-07-03 12:19:11 -0500

    Force Vivado version to 2018.3 Needed since fw 0.31

--- prep.sh
+++ prep.sh
@@ -5,3 +5,5 @@ export VIVADO_SETTINGS=/opt/Xilinx/Vivado/2018.3/settings64.sh
 # Below is needed for Ubuntu 16.04 (at least?)
 export SWT_GTK3=0
 export DISPLAY=:0
+# 0.31 complains if you have newer Vivado
+export VIVADO_VERSION=v2018.3
